2d24d41e539c2ef42d7c173f4bccc318629d5d3d
aknodell <aknodell@trinity.edu>
2016-10-11 18:58:42 -0500

    Add files via upload

--- functionalPrototype.js
+++ functionalPrototype.js
@@ -2,12 +2,13 @@ var currentMenuId;
 var menuOpen = false;
 var favorites = [];
 var products = [];
+var productCards = [];
 var productObj = {
     brand: "AXESS",
 	model: "SPBT1031-RD",
 	price: 129.99, 
 	imageUrl: "https://images-na.ssl-images-amazon.com/images/I/41u8vEGE9QL.jpg",
-	averageRating: 4, 
+	averageRating: 3.5, 
 	reviews: [
         {"rating":3,
 		"title":"Better options available for less money",
@@ -17,19 +18,13 @@ var productObj = {
 		"description":"Disappointing, for several reasons:1. Vocals are kind of muddy; sounds like the speaker is playing from inside of a cardboard box (as if the speaker was put in a box & then put on play...weird-sounding like that, like it's a bit boxed in).2. Nearly zero bass. I'm really surprised at how little bass there is; I've built Sonosubs before (subwoofers in tubes) & would have thought it would have had at least SOME bass or mid-bass in this. Nope. Stuff that's a bit more acoustic like George Ezra's \"Budapest\" sounds pretty good, but throwing on something like Soulja Boy is pretty disappointing. All that volume & no thump :(3. Doesn't get nearly as loud as I'd like. I was hoping to use this for outdoor movies with a mini LED Android projector, but the volume combined with the lack of bass is just disappointing. My little palm-sized Bluetooth speakers are nearly as loud as this is. I think part of the problem is that there's a speaker on each end, so you don't really get the full effect of a left & right speaker if you place it horizontally (the manual shows it in both horizontal & vertical configurations, with the vertical pointing the control button side up). I've found it actually sounds better standing up vertical in kind of an omni-speaker setup - not to compare it to a high-end Linkwitz, but kind of the same idea where it can at least disperse the sound a bit better. Oddly enough, it sounds best with the control button side down - gives it a throatier sound (especially for male voices, like Hozier's \"Take me to church\"), which actually points the drivers down & the \"sub\" up.On the plus side...it does work, it's a cool design (feels fairly durable!), and the price is pretty decent.Read more"}
     ],
 	favorite: false,
-	specifications: {
-		"productDimensions":"11.4 x 6.2 x 6.6 inches",
-		"itemWeight":"3.5 pounds",
-		"manufacturer":"AXESS",
-		"batteries":"1 Lithium ion batteries required. (included)"
-	}
+	specifications: {"Product Dimensions":"11.4 x 6.2 x 6.6 inches",
+        "Item Weight":"3.5 pounds",
+        "Manufacturer":"AXESS",
+        "Batteries":"1 Lithium ion batteries required. (included)"}
 };
 
-function specifications(key, value) {
-    
-}
-
-function product(brand, model, price, imageUrl, averageRating, reviews, favorite, specifications) {
+function product(brand, model, price, imageUrl, averageRating, reviews, favorite, specifications, id) {
     this.brand = brand;
 	this.model = model;
 	this.price = price;
@@ -38,14 +33,14 @@ function product(brand, model, price, imageUrl, averageRating, reviews, favorite
 	this.reviews = reviews;
 	this.favorite = favorite;
 	this.specifications = specifications;
+    this.id = id;
 }
 
 for (i = 0; i < 5; i++)
 {
-	products.push(new product("AXESS", "SPBT1031-RD", 129.99 - (10 * i), 
+	products.push(new product("AXESS", "SPBT1031-RD", 119.99 - (25 * i), 
 		"https://images-na.ssl-images-amazon.com/images/I/41u8vEGE9QL.jpg",
-		i+1, productObj.reviews, false, productObj.specifications));
-	console.log(products[i]);
+		1 + (0.5 * (i + 1)), productObj.reviews, false, productObj.specifications, "product" + i));
 }
 
 window.onload = function() {
@@ -53,86 +48,64 @@ window.onload = function() {
     for (i = 0; i < products.length; i++) {
 		var currentProduct = i;
         var productCard = document.createElement("div");
-        productCard.className += "w3-col one-col two-col three-col w3-card-2 w3-white";
-        productCard.style.height = "300px";
-        productCard.style.margin = "15px";
-        productCard.style.minWidth = "350px";
-        productCard.style.width = "100%-285px";
-        productCard.style.position = "relative";
+        productCard.className = "product-card";
         
-        productCard.id = "product" + i;
+        productCard.id = products[currentProduct].id;
 
         var container = document.createElement("div");
-        container.className += "w3-container w3-center w3-margin";
-        container.style.position = "absolute";
-        container.style.top = "0px";
-        container.style.left = "0px";
-        container.style.right = "0px";
+        container.className += "product-container";
 
         var imgContainer = document.createElement("div");
-        imgContainer.className += "w3-margin";
-        imgContainer.style.height = "150px";
+        imgContainer.className = "product-image-container";
 
         var image = document.createElement("img");
         image.src = products[currentProduct].imageUrl;
-        image.style.maxWidth = "300px";
-        image.style.maxHeight = "150px";
 
         var productName = document.createElement("div");
-        productName.className += "w3-xlarge w3-margin";
-        productName.appendChild(document.createTextNode(
-                products[currentProduct].brand + " " + products[currentProduct].model));
+        productName.className = "product-name";
+        productName.innerHTML = products[currentProduct].brand + " " + products[currentProduct].model;
 
         var starAndPriceContainer = document.createElement("div");
-        starAndPriceContainer.className += "star-and-price";
+        starAndPriceContainer.className = "product-rating-and-price-container";
         
         var stars = document.createElement("div");
-        stars.className += "w3-large";
-        stars.style.float = "left";
-        stars.style.marginLeft = "30px";
-        stars.style.color = "rgb(255, 153, 0)";
+        stars.className = "product-rating";
 
-        for (j = 0; j < 5; j++)
-        {
+        for (j = 0; j < 5; j++) {
             var star = document.createElement("i");
-            if (j < products[currentProduct].averageRating)
-            {
-                star.className += "fa fa-star";
-            }
-            else
-            {
-                star.className += "fa fa-star-o";
+            if (j + 0.5 < products[currentProduct].averageRating) {
+                star.className = "fa fa-star";
+            } else if (j + 0.5 == products[currentProduct].averageRating) {
+                star.className = "fa fa-star-half-o";
+            } else {
+                star.className = "fa fa-star-o";
             }
             stars.appendChild(star);
         }
 
         var price = document.createElement("div");
-        price.className += "w3-large";
-        price.style.float = "right";
-        price.style.marginRight = "30px";
-        price.appendChild(document.createTextNode("$"+products[currentProduct].price.toFixed(2)));
+        price.className = "product-price";
+        price.innerHTML = "$"+products[currentProduct].price.toFixed(2);
         
         var circularMenu = document.createElement("div");
-        circularMenu.className += "circular-menu";
+        circularMenu.className = "circular-menu";
         
         
         var circle = document.createElement("div");
-        circle.className += "circle";
+        circle.className = "circle";
         circle.id = "product" + i + "Menu";
         
         var favorite = document.createElement("i");
-        favorite.className += "fa fa-heart-o fa-2x";
-		favorite.style.color = "red";
+        favorite.className = "fa fa-heart-o fa-2x";
         
         var videos = document.createElement("i");
-        videos.className += "fa fa-film fa-2x";
+        videos.className = "fa fa-film fa-2x";
         
         var reviews = document.createElement("i");
-        reviews.className += "fa fa-star fa-2x";
-        reviews.style.color = "rgb(255, 153, 0)";
+        reviews.className = "fa fa-star fa-2x";
 
         var specifications  = document.createElement("i");
-        specifications.className += "fa fa-list-ul fa-2x";
+        specifications.className = "fa fa-list-ul fa-2x";
         
         var menuItems = [favorite, videos, reviews, specifications];
         for (var j = 0, l = menuItems.length; j < l; j++)
@@ -162,7 +135,6 @@ window.onload = function() {
         container.appendChild(starAndPriceContainer);
         productCard.appendChild(container);
         productCard.appendChild(circularMenu);
-        productsContainer.appendChild(productCard);
         
         productCard.oncontextmenu = (function() {
             var currentId = productCard.id;
@@ -170,6 +142,12 @@ window.onload = function() {
                 openCircularMenu(currentId + "Menu");
             }
         })();
+        
+        productCards.push(productCard);
+    }
+    
+    for (var i = 0; i < productCards.length; i++) {
+        productsContainer.appendChild(productCards[i]);
     }
 };
 
@@ -212,14 +190,119 @@ function openModal(currentProduct, menuItem)
 {
     document.getElementById(currentMenuId).classList.toggle('open');
     currentMenuId = null;
-    document.getElementById("id01").style.display="block";
+    document.getElementById("modalWindow").style.display="block";
 	populateModal(currentProduct);
 	document.getElementsByClassName("tablink")[menuItem].click();
 }
 
 function populateModal(currentProduct)
 {
-	console.log(currentProduct.averageRating);
+    document.getElementById("modalImage").src = currentProduct.imageUrl;
+    document.getElementById("modalProductName").innerHTML = currentProduct.brand + " " + currentProduct.model;
+    document.getElementById("modalPrice").innerHTML = "$"+currentProduct.price.toFixed(2);
+    for (i = 1; i <= 5; i++) {
+        var star = document.getElementById("modalStar" + i);
+        if (i <= currentProduct.averageRating) {
+            star.className = "fa fa-star";
+        } else if (i == currentProduct.averageRating + 0.5) {
+            star.className = "fa fa-star-half-o"
+        } else {
+            star.className = "fa fa-star-o";
+        }
+    }
+    
+    var specsTable = document.getElementById("modalSecificationsTable");
+    while (specsTable.firstChild) {
+        specsTable.removeChild(specsTable.firstChild);
+    }
+    for (var i in currentProduct.specifications) {
+        var row = document.createElement("tr");
+        var spec = document.createElement("td");
+        var details = document.createElement("td");
+        
+        spec.innerHTML = i;
+        details.innerHTML = currentProduct.specifications[i];
+        
+        row.appendChild(spec);
+        row.appendChild(details);
+        specsTable.appendChild(row);
+    }
+    
+    var reviews = document.getElementById("reviewsContent");
+    while (reviews.firstChild) {
+        reviews.removeChild(reviews.firstChild);
+    }
+    for (i = 0; i < currentProduct.reviews.length; i++)
+    {
+        var review = document.createElement("div");
+        review.className = "review";
+        
+        var stars = document.createElement("div");
+        stars.className = "stars";
+        for (j = 0; j < 5; j++)
+        {
+            var star = document.createElement("i");
+            if (j < currentProduct.reviews[i]["rating"]) {
+                star.className = "fa fa-star";
+            } else {
+                star.className = "fa fa-star-o";
+            }
+            stars.appendChild(star);
+        }
+        review.appendChild(stars);
+        
+        var title = document.createElement("div");
+        title.innerHTML = "<b>" + currentProduct.reviews[i]["title"] + "</b>";
+        review.appendChild(title);
+        
+        var description = document.createElement("div");
+        description.appendChild(document.createTextNode(currentProduct.reviews[i]["description"]));
+        review.appendChild(description);
+        
+        reviews.appendChild(review);
+    }
+    
+    var youtubeResultsScript = document.createElement("script");
+    youtubeResultsScript.innerHTML = 
+            "ytEmbed.init({'block':'ytThumbs','key':'AIzaSyAjRzrFIWUBkzGyca98NFNn3dfwLf6Bvh4','q':'" + currentProduct.brand + " " + 
+            currentProduct.model + "','type':'search','results':5,'meta':true,'player':'embed','layout':'full'});";
+    document.getElementById("modalVideosContainer").appendChild(youtubeResultsScript);
+}
+
+function sortProducts(sortIndex) {
+    console.log("before sort");
+    for (var i = 0; i < products.length; i++) {
+        console.log(products[i]);
+    }
+    if (sortIndex == 1) {
+        console.log("price");
+        products.sort(dynamicSort("price"));
+    } else if (sortIndex == 2) {
+        console.log("-price");
+        products.sort(dynamicSort("-price"));
+    } else if (sortIndex == 3) {
+        console.log("averageRating");
+        products.sort(dynamicSort("-averageRating"));        
+    } else if (sortIndex == 4) {
+        console.log("brand");
+        products.sort(dynamicSort("brand"));
+    }
+    console.log("after sort");
+    for (var i = 0; i < products.length; i++) {
+         console.log(products[i]);       
+    }
+}
+
+function dynamicSort(property) {
+    var sortOrder = 1;
+    if(property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+    }
+    return function (a,b) {
+        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+        return result * sortOrder;
+    }
 }
 
 function openCity(evt, cityName) {
@@ -236,14 +319,11 @@ function openCity(evt, cityName) {
   evt.currentTarget.classList.add("w3-light-grey");
 }
 
-function myAccFunc(accordionMenu) {
-    var x = document.getElementById(accordionMenu);
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-        x.previousElementSibling.className += " w3-grey";
+function toggleFilterCategory(filterCategoryId) {
+    var filterCategory = document.getElementById(filterCategoryId);
+    if (filterCategory.style.display == "block") {
+        filterCategory.style.display = "none";
     } else {
-        x.className = x.className.replace(" w3-show", "");
-        x.previousElementSibling.className =
-        x.previousElementSibling.className.replace(" w3-grey", "");
+        filterCategory.style.display = "block";        
     }
 }
